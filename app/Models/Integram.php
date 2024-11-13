2f4e1d1e5b30663cce0452830970fa34cc1ff71d
Gabi00720 <abcsi.o007@gmail.com>
2024-11-13 13:15:12 +0100

    first

---
+++ app/Models/Integram.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Integram extends Model
+{
+public $table = "integram";
+
+    protected $fillable = [
+        'cegnev',
+        'cim',
+        'adoszam',
+        'nev',
+        'tel',
+        'email',
+        'pdf'
+    ];
+}
